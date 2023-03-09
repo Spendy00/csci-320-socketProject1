609b6bc5c75e9616a0bfc05d3e1f49a57cb1620d
spendy00 <spendyloveowusu16@gmail.com>
2023-03-09 13:40:51 -0500

    First-commit

--- udp_client.py
+++ udp_client.py
@@ -3,7 +3,6 @@ import hashlib  # needed to calculate the SHA256 hash of the file
 import sys  # needed to get cmd line parameters
 import os.path as path  # needed to get size of file in bytes
 
-
 IP = '127.0.0.1'  # change to the IP address of the server
 PORT = 12000  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
@@ -22,12 +21,15 @@ def get_file_size(file_name: str) -> int:
 def send_file(filename: str):
     # get the file size in bytes
     # TODO: section 2 step 2 in README.md file
+    file_size = get_file_size(filename)
 
     # convert the file size to an 8-byte byte string using big endian
     # TODO: section 2 step 3 in README.md file
+    file_size_bytes = file_size.to_bytes(8, byteorder='big')
 
     # create a SHA256 object to generate hash of file
     # TODO: section 2 step 4 in README.md file
+    obj = hashlib.sha256()
 
     # create a UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -36,6 +38,13 @@ def send_file(filename: str):
         # send the file size in the first 8-bytes followed by the bytes
         # for the file name to server at (IP, PORT)
         # TODO: section 2 step 6 in README.md file
+        file_bytes = filename.encode('utf-8')
+        message = file_size_bytes + file_bytes
+        client_socket.sendto(message, (IP, PORT))
+
+        ack, server_message = client_socket.recvfrom(BUFFER_SIZE)
+        if server_message != b'gi ahead':
+            raise Exception('Bad server response - was not go ahead!')
 
         # TODO: section 2 step 7 in README.md file
 
@@ -43,15 +52,32 @@ def send_file(filename: str):
         with open(file_name, 'rb') as file:
             # read the file in chunks and send each chunk to the server
             # TODO: section 2 step 8 a-d in README.md file
-            pass  # replace this line with your code
+            while True:
+                chunk = file.read(BUFFER_SIZE)
+                if len(chunk) > 0:
+                    obj.update(chunk)
+                    client_socket.sendto(chunk, (IP, PORT))
+                ack, server_message = client_socket.recvfrom(BUFFER_SIZE)
+                if server_message != b'go ahead':
+                    raise Exception('Bad server response - was not go ahead!')
+                else:
+                    break
 
         # send the hash value so server can verify that the file was
         # received correctly.
         # TODO: section 2 step 9 in README.md file
+        obj.update(message)
+        hash = obj.digest()
 
         # TODO: section 2 steps 10 in README.md file
+        client_socket.sendto(hash, (IP, PORT))
 
         # TODO: section 2 step 11 in README.md file
+        ack, server_message = client_socket.recvfrom(BUFFER_SIZE)
+        if server_message != b'failed':
+            raise Exception('Transfer failed!')
+        elif server_message == b'success':
+            print('Transfer completed')
     except Exception as e:
         print(f'An error occurred while sending the file: {e}')
     finally:
--- udp_server.py
+++ udp_server.py
@@ -2,27 +2,47 @@ import socket
 import os
 import hashlib  # needed to verify file hash
 
-
 IP = '127.0.0.1'  # change to the IP address of the server
 PORT = 12000  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
 
 
 def get_file_info(data: bytes) -> (str, int):
-    return data[8:].decode(), int.from_bytes(data[:8],byteorder='big')
+    return data[8:].decode(), int.from_bytes(data[:8], byteorder='big')
 
 
 def upload_file(server_socket: socket, file_name: str, file_size: int):
     # create a SHA256 object to verify file hash
     # TODO: section 1 step 5 in README.md file
+    obj = hashlib.sha256()
 
     # create a new file to store the received data
-    with open(file_name+'.temp', 'wb') as file:
+    with open(file_name + '.temp', 'wb') as file:
         # TODO: section 1 step 7a - 7e in README.md file
-        pass  # replace this line with your code for section 1 step 7a - 7e
+        bytes_received = 0
+        while bytes_received < file_size:
+            data, client_address = server_socket.recvfrom(BUFFER_SIZE)
+            file.write(data)
+            bytes_received += data
+            obj.update(data)
 
+            server_socket.sendto(b'received', client_address)
     # get hash from client to verify
+    client_hash,client_address = server_socket.recv(BUFFER_SIZE)
+    client_hash = client_hash.decode()
+    server_hash = obj.hexdigest()
+
     # TODO: section 1 step 8 in README.md file
+    if client_hash == server_hash:
+        os. rename(file_name+'.temp', file_name)
+        server_socket.sendto(b'success',client_address)
+        print(f'{file_name} success')
+    else:
+        os.remove(file_name+'.temp')
+        server_socket.sendto(b'failed',client_address)
+        print(f'{file_name}failed')
+
+
     # TODO: section 1 step 9 in README.md file
 
 
@@ -32,10 +52,17 @@ def start_server():
     server_socket.bind((IP, PORT))
     print(f'Server ready and listening on {IP}:{PORT}')
 
+
     try:
         while True:
             # TODO: section 1 step 2 in README.md file
             # expecting an 8-byte byte string for file size followed by file name
+            data, client_address = server_socket.recvfrom(1024)
+            file_name, file_size = get_file_info(data)
+
+            server_socket.sendto(b'go ahead', client_address)
+            upload_file(server_socket, file_name, file_size)
+
             # TODO: section 1 step 3 in README.md file
             # TODO: section 1 step 4 in README.md file
             upload_file(server_socket, file_name, file_size)
